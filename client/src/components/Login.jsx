38774f5bbb7d427bd20daa3e57906c45536ec6a1
Jose Pedro <josephpedroferreira@gmail.com>
2025-07-16 15:06:25 +0100

    Login Page to Backend

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
@@ -10,21 +10,21 @@ function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:3030/api/login', {
+      const res = await fetch('http://localhost:3031/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ email, password }),
       });
 
       const data = await res.json();
 
       if (res.ok) {
         localStorage.setItem('token', data.token);
-        localStorage.setItem('username', username); // Salva o username
+        localStorage.setItem('email', email); // Salva o email
         console.log('Login successful:', data);
-        navigate(`/profile/${username}`); // Redireciona para o perfil do usuário
+        navigate(`/profile/${email}`); // Redireciona para o perfil do usuário
       } else {
         console.error('Login failed:', data.message);
         alert('Login failed: ' + data.message);
@@ -42,17 +42,17 @@ function Login() {
           <h2 className="text-2xl sm:text-3xl font-bold text-center text-white mb-6">Login</h2>
           <form onSubmit={handleLogin} className="space-y-4">
             <div>
-              <label htmlFor="username" className="block text-left text-sm font-medium text-gray-200 mb-2">
-                Username
+              <label htmlFor="email" className="block text-left text-sm font-medium text-gray-200 mb-2">
+                Email
               </label>
               <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
                 className="w-full px-3 py-2 border border-gray-800 rounded-md bg-black backdrop-blur-sm text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent"
-                placeholder="Enter your username"
+                placeholder="Enter your email"
               />
             </div>
             <div>
