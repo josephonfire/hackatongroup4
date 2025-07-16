aac9b5aea4d77df043bc8812cecb2569957e6df0
Gllp2 <laureanogllp2@gmail.com>
2025-07-16 15:04:29 +0100

    updated components

--- src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Footer() {
-  return (
-    <footer className="text-center py-4 bg-[#151623] text-[#fdfdfd]">
-      {new Date().getFullYear()} - AdCharter - Grupo 4 - Bytes4Future Hackaton
-    </footer>
-  );
-}
